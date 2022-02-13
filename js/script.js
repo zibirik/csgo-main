52ec27f73e2d3b54cd4088c58160fa1f8936baa4
Dima Skvortsov <4594651+skvorzof@users.noreply.github.com>
2022-02-14 01:01:07 +0300

    feature/csgo-menu added

--- js/script.js
+++ js/script.js
@@ -8,4 +8,39 @@ $(document).ready(function () {
 			$('.nav_sub', this).stop().fadeOut(100);         
 		}
 	);
+
+	// CSGO-menu
+	$('.map').maphilight({
+		fillColor: 'ffffff',
+		fillOpacity: 0.2,
+		stroke: false,
+	});
+
+	$('area').on('click', function() {
+		let attr = $(this).attr('data-d');
+		let msg = '';
+
+		switch (attr) {
+			case 'one':
+				msg = 'one';
+				break;
+			case 'two':
+				msg = 'two';
+				break;
+			case 'three':
+				msg = 'three';
+				break;
+			case 'four':
+				msg = 'four';
+				break;
+			case 'five':
+				msg = 'five';
+				break;
+			case 'six':
+				msg = 'six';
+				break;
+		}
+
+		alert(msg);
+	});
 });
