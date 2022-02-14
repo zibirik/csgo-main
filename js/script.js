2e865b7dd32c39a981dcb831a279199e8408f361
Dima Skvortsov <4594651+skvorzof@users.noreply.github.com>
2022-02-14 15:26:27 +0300

    add all layers

--- js/script.js
+++ js/script.js
@@ -10,6 +10,17 @@ $(document).ready(function () {
 	);
 
 	// CSGO-menu
+	let allLayer = $('#all-layer');
+	let pistolLayer = $('#pistol-layer');
+	let hardLayer = $('#hard-layer');
+	let ppLayer = $('#pp-layer');
+	let rifleLayer = $('#rifle-layer');
+	let toolsLayer = $('#tools-layer');
+	let granataLayer = $('#granata-layer');
+
+
+	let resultLayer = $('#result-layer');
+
 	$('.map').maphilight({
 		fillColor: 'ffffff',
 		fillOpacity: 0.2,
@@ -17,30 +28,72 @@ $(document).ready(function () {
 	});
 
 	$('area').on('click', function() {
-		let attr = $(this).attr('data-d');
-		let msg = '';
+		let attr = $(this).attr('data-d')
+		let txt = $(this).attr('data-t')
+
+		resultLayer.html(txt)
 
 		switch (attr) {
-			case 'one':
-				msg = 'one';
+			case '0':
+				resultLayer.html(attr)
+				allLayer.show()
+				pistolLayer.hide()
+				hardLayer.hide()
+				ppLayer.hide()
+				rifleLayer.hide()
+				toolsLayer.hide()
+				granataLayer.hide()
 				break;
-			case 'two':
-				msg = 'two';
+			case '1':
+				allLayer.hide()
+				pistolLayer.show()
+				hardLayer.hide()
+				ppLayer.hide()
+				rifleLayer.hide()
+				toolsLayer.hide()
 				break;
-			case 'three':
-				msg = 'three';
+			case '2':
+				allLayer.hide()
+				pistolLayer.hide()
+				hardLayer.show()
+				ppLayer.hide()
+				rifleLayer.hide()
+				toolsLayer.hide()
 				break;
-			case 'four':
-				msg = 'four';
+			case '3':
+				allLayer.hide()
+				pistolLayer.hide()
+				hardLayer.hide()
+				ppLayer.show()
+				rifleLayer.hide()
+				toolsLayer.hide()
 				break;
-			case 'five':
-				msg = 'five';
+			case '4':
+				allLayer.hide()
+				pistolLayer.hide()
+				hardLayer.hide()
+				ppLayer.hide()
+				rifleLayer.show()
+				toolsLayer.hide()
 				break;
-			case 'six':
-				msg = 'six';
+			case '5':
+				allLayer.hide()
+				pistolLayer.hide()
+				hardLayer.hide()
+				ppLayer.hide()
+				rifleLayer.hide()
+				toolsLayer.show()
+				break;
+			case '6':
+				allLayer.hide()
+				pistolLayer.hide()
+				hardLayer.hide()
+				ppLayer.hide()
+				rifleLayer.hide()
+				toolsLayer.hide()
+				granataLayer.show()
 				break;
 		}
-
-		alert(msg);
 	});
+
 });
