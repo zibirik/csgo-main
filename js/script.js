5a1967c53f01a01e9fde0ee88bcd45e15e5c83f2
Dima Skvortsov <4594651+skvorzof@users.noreply.github.com>
2022-02-21 18:05:31 +0300

    Refactor csgo-menu

--- js/script.js
+++ js/script.js
@@ -17,83 +17,49 @@ $(document).ready(function () {
 	let rifleLayer = $('#rifle-layer');
 	let toolsLayer = $('#tools-layer');
 	let granataLayer = $('#granata-layer');
+	let arrLayers = [allLayer, pistolLayer, hardLayer, ppLayer, rifleLayer, toolsLayer, granataLayer];
+
+	let p1 = $('#p1')
+	let p2 = $('#p2')
+	let arrObjs = [p1, p2]
 
 
-	let resultLayer = $('#result-layer');
+
+	function showLayer(layer, sector) {
+
+		if(layer == 'a' || sector == 0) {
+			$.each(arrLayers, function (index, value) {
+				if(index == sector) {
+					value.show()
+				} else {
+					value.hide()
+				}
+			})
+		}
+	}
+
+	$('area').on('click', function() {
+		let attr = $(this).attr('data-l')
+		let str = attr.split('')
+
+		showLayer(str[0], str[1])
+
+		if(str[0] != 'a') {
+			$.each(arrObjs, function (index, value) {
+
+				if(value.attr('id') == attr) {
+					value.show()
+				} else {
+					value.hide()
+				}
+			})
+		}
+	});
 
 	$('.map').maphilight({
 		fillColor: 'ffffff',
 		fillOpacity: 0.2,
 		stroke: false,
-	});
-
-	$('area').on('click', function() {
-		let attr = $(this).attr('data-d')
-		let txt = $(this).attr('data-t')
-
-		resultLayer.html(txt)
-
-		switch (attr) {
-			case '0':
-				resultLayer.html(attr)
-				allLayer.show()
-				pistolLayer.hide()
-				hardLayer.hide()
-				ppLayer.hide()
-				rifleLayer.hide()
-				toolsLayer.hide()
-				granataLayer.hide()
-				break;
-			case '1':
-				allLayer.hide()
-				pistolLayer.show()
-				hardLayer.hide()
-				ppLayer.hide()
-				rifleLayer.hide()
-				toolsLayer.hide()
-				break;
-			case '2':
-				allLayer.hide()
-				pistolLayer.hide()
-				hardLayer.show()
-				ppLayer.hide()
-				rifleLayer.hide()
-				toolsLayer.hide()
-				break;
-			case '3':
-				allLayer.hide()
-				pistolLayer.hide()
-				hardLayer.hide()
-				ppLayer.show()
-				rifleLayer.hide()
-				toolsLayer.hide()
-				break;
-			case '4':
-				allLayer.hide()
-				pistolLayer.hide()
-				hardLayer.hide()
-				ppLayer.hide()
-				rifleLayer.show()
-				toolsLayer.hide()
-				break;
-			case '5':
-				allLayer.hide()
-				pistolLayer.hide()
-				hardLayer.hide()
-				ppLayer.hide()
-				rifleLayer.hide()
-				toolsLayer.show()
-				break;
-			case '6':
-				allLayer.hide()
-				pistolLayer.hide()
-				hardLayer.hide()
-				ppLayer.hide()
-				rifleLayer.hide()
-				toolsLayer.hide()
-				granataLayer.show()
-				break;
-		}
 	});
 
 });
